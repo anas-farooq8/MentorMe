ee29f9be25ffe1562313793f6c44f4e31bcf3426
Muhammad Anas Farooq <anasfarooq8888@gmail.com>
2024-02-06 13:43:43 +0500

    Book Session Screen

---
+++ app/src/main/java/com/anasfarooq/i210813/BookSessionActivity.kt
@@ -0,0 +1,24 @@
+package com.anasfarooq.i210813
+
+import android.icu.util.Calendar
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.CalendarView
+import android.widget.Toast
+
+class BookSessionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_book_session)
+
+        val calendarView = findViewById<CalendarView>(R.id.calendarView)
+        calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
+            // month is 0-based, so add 1 for the actual month
+            val selectedDate = "$dayOfMonth/${month + 1}/$year"
+
+            // Check if the selected date is the current date and perform an action
+            val currentDate = Calendar.getInstance()
+        }
+
+    }
+}
