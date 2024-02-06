b955de0e7ebf6606bb9eea8b5d6bf26ca744eff0
Muhammad Anas Farooq <anasfarooq8888@gmail.com>
2024-02-06 21:46:30 +0500

    Audio Call Screen

---
+++ app/src/main/java/com/anasfarooq/i210813/AudioCallActivity.kt
@@ -0,0 +1,11 @@
+package com.anasfarooq.i210813
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AudioCallActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_audio_call)
+    }
+}
