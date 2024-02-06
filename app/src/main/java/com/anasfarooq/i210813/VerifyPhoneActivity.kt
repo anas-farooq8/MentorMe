90c0fa4d9e1b7a57e9b9ecb5f944d6e34f8629ba
Muhammad Anas Farooq <anasfarooq8888@gmail.com>
2024-02-06 13:43:15 +0500

    Verify Phone Screen

---
+++ app/src/main/java/com/anasfarooq/i210813/VerifyPhoneActivity.kt
@@ -0,0 +1,11 @@
+package com.anasfarooq.i210813
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VerifyPhoneActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_verify_phone)
+    }
+}
