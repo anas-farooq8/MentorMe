5e8aaebab339e2ab47a86779d21410e14405cc33
Muhammad Anas Farooq <anasfarooq8888@gmail.com>
2024-02-07 14:08:30 +0500

    Photo Screen

---
+++ app/src/main/java/com/anasfarooq/i210813/PhotoScreenActivity.kt
@@ -0,0 +1,11 @@
+package com.anasfarooq.i210813
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PhotoScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_photo_screen)
+    }
+}
