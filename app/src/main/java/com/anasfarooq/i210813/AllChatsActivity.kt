86e20cefa45ce605e24b3c1036e50aabbf1b63c1
Muhammad Anas Farooq <anasfarooq8888@gmail.com>
2024-02-08 12:59:58 +0500

    All Chats Screen

---
+++ app/src/main/java/com/anasfarooq/i210813/AllChatsActivity.kt
@@ -0,0 +1,12 @@
+package com.anasfarooq.i210813
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AllChatsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_all_chats)
+
+    }
+}
